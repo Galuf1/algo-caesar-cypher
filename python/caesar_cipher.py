88f55aad3e5ee6d261a186e5067ce3ae2057a25b
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-01 23:24:49 +0200

    solved

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,20 @@
 def caesar_cipher(string, shift_amount):
+    result = ""
+
+    for i in string:
+        code = ord(i) + shift_amount
+        if ord(i) > 64 and ord(i) < 91:
+            number = (code - 65) % 26 + 65
+            result += chr(number)
+        elif ord(i) > 96 and ord(i) < 123:
+            number = (code - 97) % 26 + 97
+            result += chr(number)
+        else: 
+            result += i
+    return result
+            
+        
+
+
+#a-z = 97-122
+#A-Z = 65-90
